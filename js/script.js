c1576b053fb582314ea842fe45142a5772216e49
bcbirdographer <122152906+bcbirdographer@users.noreply.github.com>
2023-02-12 01:11:47 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var nodes = document.querySelectorAll("#taxonomy-tree li");
-  for (var i = 0; i < nodes.length; i++) {
-    nodes[i].addEventListener("click", function(event) {
-      event.stopPropagation();
-      this.classList.toggle("open");
-    });
-  }
+// Get all nodes
+const nodes = document.querySelectorAll(".node");
+
+// Add click event listeners to all nodes
+nodes.forEach((node) => {
+  node.addEventListener("click", (event) => {
+    // Get sub-nodes of the clicked node
+    const subNodes = node.nextElementSibling;
+
+    // If sub-nodes are visible, hide them
+    if (subNodes.style.display === "block") {
+      subNodes.style.display = "none";
+    }
+    // If sub-nodes are hidden, show them
+    else {
+      subNodes.style.display = "block";
+    }
+  });
 });
