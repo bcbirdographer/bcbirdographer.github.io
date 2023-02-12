4eb216e62fbe82cc5421470574066af36536a585
bcbirdographer <122152906+bcbirdographer@users.noreply.github.com>
2023-02-11 16:59:46 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// Select all of the li elements within the #taxonomy-tree element
 const nodes = document.querySelectorAll("#taxonomy-tree li");
 
+// Loop through each node in the nodes NodeList
 nodes.forEach(node => {
-  node.addEventListener("click", function() {
-    this.classList.toggle("open");
+  // Add a click event listener to the node
+  node.addEventListener("click", () => {
+    // Toggle the open class on the clicked node
+    node.classList.toggle("open");
   });
 });
