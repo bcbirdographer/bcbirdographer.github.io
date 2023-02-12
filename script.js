254357b3942ac6c3c5c9729ebfe5e0f8bc6fb63a
bcbirdographer <122152906+bcbirdographer@users.noreply.github.com>
2023-02-11 16:43:30 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const nodes = document.querySelectorAll("#taxonomy-tree li");
-
-nodes.forEach(node => {
-  node.addEventListener("click", function() {
-    this.classList.toggle("open");
-  });
-});
