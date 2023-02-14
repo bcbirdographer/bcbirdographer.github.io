9a8d87ca541742ba1d08d40b7f64a0f0719a282b
bcbirdographer <122152906+bcbirdographer@users.noreply.github.com>
2023-02-14 14:00:42 -0800

    rearrangements

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,72 +1,9 @@
-const treeData = [
-  {
-    text: "Parent Node 1",
-    nodes: [
-      {
-        text: "Child Node 1",
-        nodes: [
-          {
-            text: "Grandchild Node 1"
-          },
-          {
-            text: "Grandchild Node 2"
-          }
-        ]
-      },
-      {
-        text: "Child Node 2"
-      }
-    ]
-  },
-  {
-    text: "Parent Node 2",
-    nodes: [
-      {
-        text: "Child Node 1"
-      },
-      {
-        text: "Child Node 2",
-        nodes: [
-          {
-            text: "Grandchild Node 1"
-          },
-          {
-            text: "Grandchild Node 2"
-          }
-        ]
-      }
-    ]
-  }
-];
-
-const tree = document.querySelector(".tree");
-const treeNodes = document.querySelectorAll(".node");
-
-treeNodes.forEach(node => {
-  node.addEventListener("click", e => {
-    const parentNode = e.currentTarget.parentNode;
-    const subNodes = parentNode.querySelector(".nodes");
-    subNodes.classList.toggle("active");
-  });
-});
-
-const createTree = data => {
-  let treeHTML = `<ul class="nodes">`;
-  data.forEach(node => {
-    treeHTML += `
-      <li class="node">
-        <div class="node-header">
-          <i class="fas fa-plus"></i>
-          <span>${node.text}</span>
-        </div>
-    `;
-    if (node.nodes) {
-      treeHTML += createTree(node.nodes);
-    }
-    treeHTML += `</li>`;
-  });
-  treeHTML += `</ul>`;
-  return treeHTML;
+const toggleNode = node => {
+  node.nextElementSibling.classList.toggle("expanded");
 };
 
-tree.innerHTML = createTree(treeData);
+const parentNode = document.querySelector("li");
+parentNode.addEventListener("click", () => toggleNode(parentNode));
+
+// Add the following code to set the initial state of the tree diagram
+document.querySelector("ul.child-nodes").style.display = "none";
--- docs/index.md
+++ docs/index.md
@@ -1,48 +1,12 @@
 ---
 layout: default
-title: Tree Diagram
 ---
 
-<div class="tree-container">
-  <div class="node">
-    <div class="logo">
-      <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-    </div>
-    <div class="children">
-      <div class="node">
-        <div class="logo">
-          <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-        </div>
-        <div class="children">
-          <div class="node">
-            <div class="logo">
-              <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-            </div>
-          </div>
-          <div class="node">
-            <div class="logo">
-              <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="node">
-        <div class="logo">
-          <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-        </div>
-        <div class="children">
-          <div class="node">
-            <div class="logo">
-              <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-            </div>
-          </div>
-          <div class="node">
-            <div class="logo">
-              <img src="{{ "/assets/logo.png" | relative_url }}" alt="Logo">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+<ul id="tree">
+  <li><img src="/assets/logo.png" alt="logo"></li>
+  <ul class="child-nodes">
+    <li>Child Node 1.1</li>
+    <li>Child Node 1.2</li>
+    <li>Child Node 1.3</li>
+  </ul>
+</ul>
